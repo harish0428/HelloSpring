f31f0528935bc313cb55b517d46d1a900a724349
Harish <harish@example.com>
2017-10-23 20:36:03 +0000

    Updated source file

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+        @RequestMapping(value="/")
+        public String Demo(){
+            return "Welcome to DockerBuild";
+   }
 
-}
+ }
